dba9063bf0ba2ac5a43a13c56cecfaa914dc9dcf
Bhumi <bhumiaggarwal386@gmail.com>
2026-04-13 17:37:34 +0530

    repository

---
+++ app/src/main/java/com/example/bhumicse/ClothingRepository.kt
@@ -0,0 +1,13 @@
+package com.example.bhumicse
+
+import kotlinx.coroutines.flow.Flow
+
+class ClothingRepository(private val clothingDao: ClothingDao) {
+
+    // This will give us a list of all clothes that updates automatically
+    val allClothes: List<ClothingItem> = clothingDao.getAllClothes()
+
+    suspend fun insert(item: ClothingItem) {
+        clothingDao.addClothes(item)
+    }
+}
